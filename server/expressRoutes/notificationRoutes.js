8c969438d55b791d2417010945a9235eec0c9f8b
weilun <48602497+noobsonli@users.noreply.github.com>
2020-03-18 14:11:53 +0800

    notification update

--- server/expressRoutes/notificationRoutes.js
+++ server/expressRoutes/notificationRoutes.js
@@ -42,7 +42,7 @@ notificationRoutes.route('/:id').put(function(req,res){
 });
 
 
-//read notification
+//read notification by id
 notificationRoutes.route('/:id').get(function(req,res){
     Notification.findById({_id: req.params.id},function(err,notification){
 
@@ -53,5 +53,60 @@ notificationRoutes.route('/:id').get(function(req,res){
 
 });
 
+//read notification of one customer
+notificationRoutes.route('/customer').get(function(req,res){
+    var customerId = req.body.id;
+    var extractedId = req.id;
+
+    if(extractedId != customerId){
+        res.status(401).send('Unauthorized user');
+        return;
+    }
+
+    Notification.find(customerId, function(err, notification){
+        if (err) {
+            console.log(err);
+            res.send.json('An error occurs!');
+        }
+        res.json(notification);
+    })
+})
+
+//read notification of one vendor
+notificationRoutes.route('/vendor').get(function(req,res){
+    var vendorId = req.body.id;
+    var extractedId = req.id;
+
+    if(extractedId != vendorId){
+        res.status(401).send('Unauthorized user');
+        return;
+    }
+
+    Notification.find(vendorId, function(err, notification){
+        if (err) {
+            console.log(err);
+            res.send.json('An error occurs!');
+        }
+        res.json(notification);
+    })
+})
+//read notification of one pet
+notificationRoutes.route('/pet').get(function(req,res){
+    var petId = req.body.id;
+    var extractedId = req.id;
+
+    if(extractedId != petId){
+        res.status(401).send('Unauthorized user');
+        return;
+    }
+
+    Notification.find(petId, function(err, notification){
+        if (err) {
+            console.log(err);
+            res.send.json('An error occurs!');
+        }
+        res.json(notification);
+    })
+})
 
 module.exports = notificationRoutes;  
