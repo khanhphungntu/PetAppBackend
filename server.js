8bc0de6bf5b432169345f7178278b85a2ff32d45
cuongquangnam <42933311+cuongquangnam@users.noreply.github.com>
2020-03-22 01:26:52 +0800

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ const customerRoutes = require('./server/expressRoutes/customerRoutes');
 const accountRoutes = require('./server/expressRoutes/accountRoutes');
 const vendorRoutes = require('./server/expressRoutes/vendorRoutes');
 const petRoutes = require('./server/expressRoutes/petRoutes');
+const scheduleRoutes = require('./server/expressRoutes/scheduleRoutes');
+const notificationRoutes = require('./server/expressRoutes/notificationRoutes');
 const vendorLocationRoutes = require('./server/expressRoutes/vendorLocationRoutes');
+
 // connect app to mongoDB
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/petApp').then(
@@ -29,10 +32,12 @@ app.use(cors());
 //Mapping Express Route with Server Route
 app.use('/api/account', accountRoutes);
 app.use('/api/auth', authRoutes);
-app.use('/api/services', withAuth, serviceRoutes);
+app.use('/api/service', withAuth, serviceRoutes);
 app.use('/api/customer', withAuth, customerRoutes);
 app.use('/api/vendor', withAuth, vendorRoutes);
-app.use('/api/pet',petRoutes);
+app.use('/api/pet',withAuth, petRoutes);
+app.use('/api/notification',withAuth, notificationRoutes);
+app.use('/api/schedule', scheduleRoutes);
 app.use('/api/vendorLocation', vendorLocationRoutes);
 const port = process.env.PORT || 4000;
 const server = app.listen(port, function(){
