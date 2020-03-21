094b9d4a4ff6aea3956ee80495a4094bf0db1822
cuongquangnam <tccuong1999@gmail.com>
2020-03-22 00:59:41 +0800

    done with longitude, latitude and vendorLocation

--- server/models/vendorLocation.js
+++ server/models/vendorLocation.js
@@ -3,11 +3,18 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 // Define collection and schema for Items
-var vendorLocation = new Schema({
-  
-  petId: String,
-},{
-  collection: 'vendorLocations'
+var vendorLocation = new Schema(
+  {
+    name:String,
+    type:String,
+    address:String,
+    postal_code: Number,
+    tel_office_1:Number,
+    tel_office_2:Object,
+    fax_office: String,
+    vendorId: String,
+    },{
+  collection: 'vendorLocation'
 });
 
 module.exports = mongoose.model('vendorLocation', vendorLocation);
