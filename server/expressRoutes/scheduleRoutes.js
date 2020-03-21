18239954576fdeabbf80bb3427ed5de35ad3c21d
weilun <48602497+noobsonli@users.noreply.github.com>
2020-03-21 19:49:35 +0800

    schedule done

--- server/expressRoutes/scheduleRoutes.js
+++ server/expressRoutes/scheduleRoutes.js
@@ -6,30 +6,52 @@ var Schedule = require('../models/unavailableDate');
 scheduleRoutes.route('/add').post(function(req,res){
     var schedule = new Schedule(req.body);
 
-    var id = req.body.id;
-    
-    Schedule.count({vendorId : schedule.vendorId}, function(err,count){
-        if (count > 0){
-            res.json(err);
-            Schedule.count({date: schedule.date},function(err, count1){
-                if (count1>0){
-                    if (err) res.json(err);
-                    res.status(400).send('Vendor is already booked!');
-                    return;
+    Schedule.exists({vendorId : schedule.vendorId}, function(err,result){
+            if (err){
+                res.send(err);
+            }
+            else {
+                if (result == true){
+                    Schedule.exists({date: schedule.date},function(err,result){
+                        if (err){
+                            res.send(err);
+                        }
+                        else{
+                            if (result == true)
+                            res.json("Date is already booked!");
+                            else{
+                                schedule.save()
+                                .then(item => {
+                                res.status(200).json({item});
+                                })
+                                .catch(err => {
+                                res.status(400).send("unable to save to database");
+                                });
+                            }
+                        }
+                    })
                 }
-                schedule.save()
-                .then(item => {
-                    res.status(200).json({item});
-                })
-                .catch(err => {
-                    res.status(400).send("unable to save to database");
-                });
-            })
-        }
-    })
+            }   
+        })
 });
 
+//get booking by vendorID
+scheduleRoutes.get('/').get(function(req,res){
+    var id = req.body.id;
+    var extractedId = req.id;
 
+    if(extractedId != id){
+        res.status(401).send('Unauthorized user');
+        return;
+    }
+
+    Schedule.find({vendorId: vendorId},function(err,schedule){
+        if (err) res.json(err);
+        else res.json(schedule);
+    })
+})
+
+//delete by id
 scheduleRoutes.route('/:id').delete(function(req,res){
     var id = req.body.id;
     var extractedId = req.id;
@@ -50,4 +72,6 @@ scheduleRoutes.route('/:id').delete(function(req,res){
         return;
     }
 })
+
+
 module.exports = scheduleRoutes;  
