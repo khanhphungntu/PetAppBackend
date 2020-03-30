23294db54c6def848c2d179421eb22ad9580b20d
weilun <48602497+noobsonli@users.noreply.github.com>
2020-03-30 16:34:38 +0800

    schedule

--- server/expressRoutes/scheduleRoutes.js
+++ server/expressRoutes/scheduleRoutes.js
@@ -6,114 +6,90 @@ var Notification = require('../models/notification');
 
 
 //set vendor
-scheduleRoutes.route('/add').post(function(req,res){
+scheduleRoutes.route('/add').post(function (req, res) {
     var schedule = new Schedule(req.body);
-    var existvendor = false;
 
-    Schedule.exists({vendorId : schedule.vendorId}, function(err,result){
-        if (err){
+    Schedule.exists({ vendorId: schedule.vendorId, date: schedule.date }, function (err, result) {
+        if (err) {
             res.send(err);
         }
         else {
-            if (result == true){
-                existvendor = true;
-                Schedule.exists({date: schedule.date},function(err,result){
-                    if (err){
-                        res.send(err);
-                    }
-                    else{
-                        if (result == true){
-                            //same vendor same date
-                            res.json("Date is already booked!");
+            if (result == true) {
+                //same vendor same date
+                res.json("Date is already blocked!");
 
-                            //cancel booking
-                            Booking.findOne({vendorId : schedule.vendorId}, function(err,booking){
-                                
-                                booking.status = "cancelled";
-                                booking.save();
-                                var notification = new Notification();
-                                notification.vendorId = booking.vendorId;
-                                notification.customerId = booking.customerId;
-                                notification.bookingStatus = "cancelled";
-                                notification.bookingId = booking.id;
-                                notification.save()
-                                .catch(err => {
-                                console.log(err);
-                                res.status(400).send("Unable to save to database");
-                                })
-                            })
-                            return;
-                            }
-                        else{
-                            //same vendor different date
-                            schedule.save()
-                            .then(item => {
-                            res.status(200).json({item});
-                            })
-                            .catch(err => {
-                            res.status(400).send("unable to save to database");
-                            });
-                        }
-                    }
+                //cancel booking when date is blocked
+                Booking.findOne({ vendorId: schedule.vendorId }, function (err, booking) {
+                    booking.status = "cancelled";
+                    booking.save()
+                        .catch(err => {
+                            console.log(err);
+                            res.status(400).send("Unable to save to database");
+                        })
+                    var notification = new Notification();
+                    notification.vendorId = booking.vendorId;
+                    notification.customerId = booking.customerId;
+                    notification.bookingStatus = "cancelled";
+                    notification.bookingId = booking.id;
+                    notification.save()
+                        .catch(err => {
+                            console.log(err);
+                            res.status(400).send("Unable to save to database");
+                        })
                 })
             }
-            //different vendor
-            if (existvendor == false){
-            schedule.save()
-            .then(item => {
-            res.status(200).json({item});
-            })
-            .catch(err => {
-            res.status(400).send("unable to save to database");
-            });
+            else {
+                schedule.save()
+                    .then(item => {
+                        res.status(200).json({ item });
+                    })
             }
-            else return;
-        }   
+        }
     })
-});
-    
+})
+
 scheduleRoutes.route('/tmp').post((req, res) => {
     var schedule = new Schedule(req.body);
     schedule.save()
-    .then(item => {
-        res.json('ok')
-    })
+        .then(item => {
+            res.json('ok')
+        })
 })
 //get unavailable dates by id
-scheduleRoutes.route('/:id').get(function(req,res){
+scheduleRoutes.route('/:id').get(function (req, res) {
     var id = req.params.id;
 
-    Schedule.findById({_id: id},function(err,unavailableDate){
+    Schedule.findById({ _id: id }, function (err, unavailableDate) {
         if (err) res.json(err);
         else res.json(unavailableDate);
     })
 })
 
 //get unavailable dates by vendorID
-scheduleRoutes.route('/vendor/:id').get(function(req,res){
+scheduleRoutes.route('/vendor/:id').get(function (req, res) {
     var id = req.params.id;
     var extractedId = req.id;
 
-    if(extractedId != id){
+    if (extractedId != id) {
         res.status(401).send('Unauthorized user');
         return;
     }
 
-    Schedule.find({vendorId: id},function(err,schedule){
+    Schedule.find({ vendorId: id }, function (err, schedule) {
         if (err) res.json(err);
         else res.json(schedule);
     })
 })
 
 //delete by id
-scheduleRoutes.route('/:id').delete(function(req,res){
+scheduleRoutes.route('/:id').delete(function (req, res) {
     var id = req.params.id;
 
-        Schedule.findByIdAndDelete(id, function(err,schedule){
-            if (err) res.json(err);
-            else res.json('Succesfully removed');
-        })
-    
+    Schedule.findByIdAndDelete(id, function (err, schedule) {
+        if (err) res.json(err);
+        else res.json('Succesfully removed');
+    })
+
 
 })
 
