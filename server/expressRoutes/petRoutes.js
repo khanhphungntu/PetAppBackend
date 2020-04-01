f13f0b69cd87199c7e1a9b45c2dfb669030e8408
cuongquangnam <42933311+cuongquangnam@users.noreply.github.com>
2020-04-01 17:52:43 +0800

    Update petRoutes.js

--- server/expressRoutes/petRoutes.js
+++ server/expressRoutes/petRoutes.js
@@ -24,7 +24,11 @@ petRoutes.route('/:id').delete((req, res) => {
 
     Pet.findById(id, (err, pet) => {
 
-        if (!pet || err) return next(new Error('Could not load Document'));
+
+        if (!pet || err){
+            res.status(400).json("Pet not found or " + err);
+            return;
+        }
         else {
             if (pet.deletedAt != null) {
                 res.json("Pet is already deleted!");
