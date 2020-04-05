aadbfc04ee31b67a629c88dd0cc77ce0282c29fa
cuongquangnam <tccuong1999@gmail.com>
2020-04-05 17:25:10 +0800

    change get vendorlocation

--- server/expressRoutes/vendorLocationRoutes.js
+++ server/expressRoutes/vendorLocationRoutes.js
@@ -1,35 +1,48 @@
-var express = require('express')
+var express = require("express");
 var vendorLocationRoutes = express.Router();
-var VendorLocation = require('../models/vendorLocation');
+var VendorLocation = require("../models/vendorLocation");
 
 //get all Vendor Locations
-vendorLocationRoutes.route("").get( (req,res)=>{
-    VendorLocation.find().sort({name:1}).then((data)=>{
+vendorLocationRoutes.route("").get((req, res) => {
+  VendorLocation.find()
+    .sort({ name: 1 })
+    .then((result) => {
+        let data = [] 
+        for (let i of result){
+            if (i.vendorId)
+                data.push(i)
+        }
 
-        res.status(200).json(data);
-    });
-})
+        for (let i of result){
+            if (!i.vendorId)
+                data.push(i)
+        }
+
+        res.status(200).json(data)
+    })
+
+
+});
 
 //get vendor Location by name
-vendorLocationRoutes.route("/:name").get((req,res)=>{
-    VendorLocation.find({name:req.params.name},(err,data)=>{
-        res.status(200).json(data);
-    })
-})
+vendorLocationRoutes.route("/:name").get((req, res) => {
+  VendorLocation.find({ name: req.params.name }, (err, data) => {
+    res.status(200).json(data);
+  });
+});
 
 //get vendor Location by Id
-vendorLocationRoutes.route("/vendor/:id").get((req,res)=>{
-    VendorLocation.findOne({vendorId: req.params.id},(err,data)=>{
-
-        if(!err)res.status(200).json(data);
-        else res.status(400).json("Error when finding vendor Location by Id")
-    })
-})
+vendorLocationRoutes.route("/vendor/:id").get((req, res) => {
+  VendorLocation.findOne({ vendorId: req.params.id }, (err, data) => {
+    if (!err) res.status(200).json(data);
+    else res.status(400).json("Error when finding vendor Location by Id");
+  });
+});
 
 // vendorLocationRoutes.route("/deleteNa").put((req,res)=>{
 //     VendorLocation.find({},(err,locations)=>{
 //         console.log("Yesss")
-//         locations.forEach((location)=>       
+//         locations.forEach((location)=>
 //         {
 //              if (location['tel_office_2']==='na')
 //         {
@@ -40,7 +53,6 @@ vendorLocationRoutes.route("/vendor/:id").get((req,res)=>{
 //             console.log("Yesssss")
 //         }
 //         console.log("location: "+location['tel_office_2'])
-
 
 //         }
 //         )
