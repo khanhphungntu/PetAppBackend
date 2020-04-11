dfba69262a64b7d0ff58e007fe89e4da2ef3c09f
cuongquangnam <tccuong1999@gmail.com>
2020-04-11 15:49:49 +0800

    add a file

---
+++ server/expressRoutes/serviceNotificationRoutes.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var serviceNotificationRoutes = express.Router();
+var ServiceNotification = require('../models/serviceNotification');
+
+serviceNotificationRoutes.route('/').post((req, res) => {
+
+    var userId = req.id;
+    ServiceNotification.findOne({userId: userId}, (err, user) => {
+        if(err){
+            console.log(err);
+            res.status(400).send("Bad request!")
+        }
+        else{
+            if(user){
+                let devices = user.deviceId;
+                if(devices.includes(req.body.deviceId)){
+                    res.status(200).send('Device is registered!')
+                    return;
+                }
+                devices.push(req.body.deviceId);
+
+                user.update({deviceId: devices}, (err, user) => {
+                    if(err){
+                        console.log(err);
+                        res.status(400).send("Unable to update database")
+                    }
+                    else{
+                        res.status(200).send('Device is registered!')
+                    }
+                } )
+            }
+            else{
+                let serviceNotification = new ServiceNotification({
+                    userId: userId,
+                    deviceId: [req.body.deviceId]
+                })
+
+                serviceNotification.save((err) => {
+                    if(err){
+                        console.log(err);
+                        res.status(400).send("Unable to update database")
+                    }
+                    else{
+                        res.status(200).send('Device is registered!')
+                    }
+                })
+            }
+        }
+    })
+})
+
+module.exports = serviceNotificationRoutes;
